d610f1763bbf9228bd41e3e463a10f75f3fad899
unknown <arjablc@gmail.com>
2024-08-27 14:07:21 +0545

    Revert "before cp mason to lspconfig"
    going back to separate mason and lspconfig config

--- lua/nvim/knot/plugins/lsp_n_cmp/mason.lua
+++ lua/nvim/knot/plugins/lsp_n_cmp/mason.lua
@@ -1,40 +1,35 @@
 return {
-  {
-    'WhoIsSethDaniel/mason-tool-installer.nvim',
-  },
-  {
-    'williamboman/mason-lspconfig.nvim',
-  },
-  {
-  'williamboman/mason.nvim',
-  config = function()
-    local masonlsp = require 'mason-lspconfig'
-    local masontools = require 'mason-tool-installer'
-    local mason = require 'mason'
-    mason.setup {
-      ui = {
-        icons = {
-          package_installed = '✓',
-          package_pending = '➜',
-          package_uninstalled = '✗',
-        },
-      },
-    }
-    masonlsp.setup {
-      ensure_installed = {
-        'lua_ls', --lsp for lua
-        'tsserver', -- typescript
-      },
-    }
-    masontools.setup {
-      ensure_installed = {
-        'prettier', -- prettier formatter
-        'stylua', -- lua formatter
-        'eslint_d', -- for js
-        -- "isort", -- python formatter
-        -- "black", -- python formatter
-      },
-    }
-  end, 
-  }
+	"williamboman/mason.nvim",
+	dependencies = {
+		"WhoIsSethDaniel/mason-tool-installer.nvim",
+		"williamboman/mason-lspconfig.nvim",
+	},
+	config = function()
+		local masonlsp = require("mason-lspconfig")
+		local mason = require("mason")
+		local masontools = require("mason-tool-installer")
+		mason.setup({
+			ui = {
+				icons = {
+					package_installed = "✓",
+					package_pending = "➜",
+					package_uninstalled = "✗",
+				},
+			},
+		})
+		masonlsp.setup({
+			ensure_installed = {
+				"lua_ls", --lsp for lua
+			},
+		})
+		masontools.setup({
+			ensure_installed = {
+				"prettier", -- prettier formatter
+				"stylua", -- lua formatter
+				"eslint_d",
+				-- "isort", -- python formatter
+				-- "black", -- python formatter
+			},
+		})
+	end,
 }
--- lua/nvim/knot/plugins/neotree.lua
+++ lua/nvim/knot/plugins/neotree.lua
@@ -1,25 +1,29 @@
 return {
-  'nvim-neo-tree/neo-tree.nvim',
-  branch = 'v3.x',
-  dependencies = {
-    'nvim-lua/plenary.nvim',
-    'nvim-tree/nvim-web-devicons',
-    'MunifTanjim/nui.nvim',
-  },
-  config = function()
-    require('neo-tree').setup {
+	"nvim-neo-tree/neo-tree.nvim",
+	branch = "v3.x",
+	dependencies = {
+		"nvim-lua/plenary.nvim",
+		"nvim-tree/nvim-web-devicons",
+		"MunifTanjim/nui.nvim",
+	},
+	config = function()
+
+    require('neo-tree').setup({
       window = {
         fuzzy_finder_mappings = {
-          ['<C-j'] = 'move_cursor_down',
-          ['<C-k'] = 'move_cursor_up',
-        },
-      },
-    }
+          ["<C-j"] = "move_cursor_down",
+          ["<C-k"] = "move_cursor_up",
+        }
+      }
+
+    })
+
 
     local keymap = vim.keymap -- for conciseness
 
-    keymap.set('n', '<leader>ee', '<cmd>Neotree show filesystem left toggle<CR>', { desc = 'Toggle file explorer' }) -- toggle file explorer
-    keymap.set('n', '<leader>ef', '<cmd>Neotree focus reveal <CR>', { desc = 'Toggle file explorer on current file' }) -- toggle file explorer on current file
-    keymap.set('n', '<leader>eg', '<cmd>Neotree show git_status toggle<CR>', { desc = 'show git status' })
-  end,
+    keymap.set("n", "<leader>ee", "<cmd>Neotree show filesystem left toggle<CR>", { desc = "Toggle file explorer" }) -- toggle file explorer 
+    keymap.set("n", "<leader>ef", "<cmd>Neotree focus reveal <CR>", { desc = "Toggle file explorer on current file" }) -- toggle file explorer on current file
+	end,
 }
+
+
